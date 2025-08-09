1c0f87c00b98aaf39d4c309eb68b37ee4eb1f170
mirana223 <emirkolaylik@gmail.com>
2025-08-09 20:27:05 +0300

    Update duck.py

--- M2L1/duck.py
+++ M2L1/duck.py
@@ -7,4 +7,21 @@ def get_duck_image_url():
     data = res.json()
     return data['url']
 
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_pok_image_url():    
+    url = 'https://pokeapi.co'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
 
